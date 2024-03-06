72ea039a4dc88e4603c8ec1c048020d1e4cbf084
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-06 11:01:32 +0100

    OuterVarriableTrap

---
+++ OuterVarriableTrap/OuterVarriableTrap/Program.cs
@@ -0,0 +1,17 @@
+﻿class TextCalss {
+
+    static void Main(String[] args) {
+
+        Action szamkiiro = null;
+        for (int i = 0; i < 10; i++) {
+            int f = i;
+            szamkiiro += delegate () {
+                //Console.WriteLine(i);
+                Console.WriteLine(f);
+            };
+        }
+        szamkiiro();
+    }
+
+
+}
